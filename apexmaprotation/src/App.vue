1be55b3bb40a4df5264fe0f437d04ac567fd97cb
Serdar-Kaya <k.serdar.sk12@gmail.com>
2021-11-07 19:59:52 +0300

    hallelujah

--- apexmaprotation/src/App.vue
+++ apexmaprotation/src/App.vue
@@ -1,27 +1,9 @@
 <script setup>
-import axios from "axios";
-
-const getData = axios
-  .get(
-    "https://api.mozambiquehe.re/maprotation?version=2&auth=1VzhbvWZZXKKWP5q9mux"
-  )
-  .then((response) => {
-    console.log(response.data.battle_royale.current.map);
-  });
+import Main from "./components/Main.vue";
 </script>
 
 <template>
-  <p></p>
-  <div><h1>BATTLE ROYALE</h1></div>
+  <div id="app">
+    <Main></Main>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ apexmaprotation/src/components/Main.vue
@@ -0,0 +1,29 @@
+<script setup>
+import axios from "axios";
+
+const getData = axios
+  .get(
+    "https://api.mozambiquehe.re/maprotation?version=2&auth=1VzhbvWZZXKKWP5q9mux"
+  )
+  .then((response) => {
+    console.log(response.data.battle_royale.current.map);
+  });
+</script>
+
+<template>
+  <p></p>
+  <div><h1>BATTLE ROYALE</h1></div>
+  <p>nice</p>
+  <div></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
